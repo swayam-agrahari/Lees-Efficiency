2dbcc83f4eff1f2a11733763ee336633ba48f41a
amansxcalibur <notifyamanvs@gmail.com>
2024-11-24 04:20:36 +0530

    added contact us form

--- app/(lander)/page.js
+++ app/(lander)/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import LandingPage from "../../components/LandingPage";
 import Expertise from "@/components/expertise/expertise";
+import ContactUs from "@/components/contact-us/contact_us";
 
 export default function Home() {
   return (
     <div className="w-full h-full bg-[#1c1c1c] text-black min-h-[300vh]">
       {/* <LandingPage/> */}
       <Expertise/>
+      <ContactUs/>
     </div>
   );
 }
