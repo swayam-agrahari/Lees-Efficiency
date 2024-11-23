d2f7227aa14b906a2a8ad4193529bd53f3556290
amansxcalibur <notifyamanvs@gmail.com>
2024-11-24 03:52:56 +0530

    added description onclick with json data (no images)

--- components/expertise/expertise.jsx
+++ components/expertise/expertise.jsx
@@ -1,16 +1,23 @@
-import Link from "next/link";
-import CornerArrow from "../svg/corner-arrow";
+'use client'
+
+import { useState } from "react";
 import ExportiseCard from "./expertise_card";
 import expertise_data from "@/components/json/expertise.json";
+import ExpertiseBrief from "./expertise_brief";
 
 export default function Expertise() {
+  const [open, setOpen]=useState(false);
+  const [brief, setBrief]=useState({});
   return (
     <div className="bg-black text-black w-full min-h-screen flex flex-col items-center">
       <p className="text-[2vw] text-white w-full max-w-[90vw] mb-[3vw] mt-[5vw] flex">Expertise</p>
       <div className="grid max-w-[90vw] grid-cols-4 gap-[2vw]">
         {expertise_data.map((data, index) => (
-          <ExportiseCard key={index} data={data} />
+          <ExportiseCard key={index} data={data} setOpen={setOpen} open={open} setBrief={setBrief} />
         ))}
+      </div>
+      <div className={`${open?'':'hidden'} backdrop-blur-lg min-h-screen fixed z-10 top-0 w-full text-white pt-[5vw] flex justify-center`}>
+        <ExpertiseBrief setOpen={setOpen} brief={brief}/>
       </div>
     </div>
   );
--- components/svg/x.jsx
+++ components/svg/x.jsx
@@ -1,7 +1,7 @@
 export default function X(){
     return(
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-        className="fill-current text-black hover:text-blue-400 size-[2.1vw] max-md:text-[10vw] max-md:size-10  before: hover:border-blue-400 border-2 border-black rounded-full">
+        className="fill-current text-[#737373] hover:text-white size-[2.1vw] max-md:text-[10vw] max-md:size-10  before: hover:border-[#1c1c1c] border-2 border-black rounded-full">
             <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
         </svg>
     )
