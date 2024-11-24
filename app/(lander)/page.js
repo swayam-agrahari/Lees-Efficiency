6c2bb23df7e6424b016f60d27b3cd7c0674e735f
amansxcalibur <notifyamanvs@gmail.com>
2024-11-24 18:39:17 +0530

    added contact footer skeleton

--- app/(lander)/page.js
+++ app/(lander)/page.js
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import LandingPage from "../../components/LandingPage";
 import Expertise from "@/components/expertise/expertise";
-import ContactUs from "@/components/contact-us/contact_us";
+import ContactUsForm from "@/components/contact-us/contact_us_form";
+import ContactUsFooter from "@/components/contact-us/contact_us_footer";
 
 export default function Home() {
   return (
     <div className="w-full h-full bg-[#1c1c1c] text-black min-h-[300vh]">
       {/* <LandingPage/> */}
       <Expertise/>
-      <ContactUs/>
+      <ContactUsForm/>
+      <ContactUsFooter/>
     </div>
   );
 }
---
+++ components/contact-us/contact_us_footer.jsx
@@ -0,0 +1,33 @@
+export default function ContactUs(){
+    return(
+        <div className="bg-red-600 min-h-[30vw]">
+            <div className="">LEEs</div>
+            <div className="bg-gray-400 w-full flex">
+                <div className="flex-1">
+                    <p>Quick Links</p>
+                    <div>
+                        <p>Home</p>
+                        <p>About Us</p>
+                        <p>Expertise</p>
+                        <p>Lees Academy</p>
+                        <p>Awards</p>
+                        <p>Contact Us</p>
+                        <p>Privacy Policy</p>
+                        <p>Terms & Privacy</p>
+                    </div>
+                </div>
+                <div className="flex-1">
+                    <p>Contact Information</p>
+                    <div>
+                        <p>LEES ENERGY SOLUTIONS PTE. LTD.</p>
+                        <p>1 North Brigade Road, #18-03,</p>
+                        <p>High Street Centre,</p>
+                        <p>Singapore 179094.</p>
+                        <p>+65 83166856</p>
+                        <p>leelock@leesenergysolutions.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
