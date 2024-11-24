3aa5f5dff0c07b172e49341cc0dd1efc277b9e6b
amansxcalibur <notifyamanvs@gmail.com>
2024-11-24 20:34:24 +0530

    added styles to the contact footer

--- app/(lander)/page.js
+++ app/(lander)/page.js
@@ -6,10 +6,16 @@ import ContactUsFooter from "@/components/contact-us/contact_us_footer";
 
 export default function Home() {
   return (
-    <div className="w-full h-full bg-[#1c1c1c] text-black min-h-[300vh]">
+    <div className="w-full h-full text-black min-h-[300vh] flex flex-col bg-black">
       {/* <LandingPage/> */}
       <Expertise/>
+      <div className="w-full px-[5vw] my-[5vw]">
+        <div className="border-t-[#737373] border-solid border-t-2 max-w-[90vw]"></div>
+      </div>
       <ContactUsForm/>
+      <div className="w-full px-[5vw] mt-[5vw] mb-[1vw]">
+        <div className="border-t-[#737373] border-solid border-t-2 max-w-[90vw]"></div>
+      </div>
       <ContactUsFooter/>
     </div>
   );
