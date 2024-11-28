f1eb48a916dee4c34be8898d5ae003a5bff3e925
Swayam Agrahari <swayamagrahari025@gmail.com>
2024-11-28 08:56:41 +0530

    feat:expertise revamp

--- components/expertise/expertise.jsx
+++ components/expertise/expertise.jsx
@@ -6,18 +6,29 @@ import expertise_data from "@/components/json/expertise.json";
 import ExpertiseBrief from "./expertise_brief";
 
 export default function Expertise() {
-  const [open, setOpen]=useState(false);
-  const [brief, setBrief]=useState({});
+  const [open, setOpen] = useState(false);
+  const [brief, setBrief] = useState({});
+
   return (
-    <div className="bg-black text-black w-full min-h-screen flex flex-col items-center">
-      <p className="text-[2vw] text-white w-full max-w-[90vw] mb-[3vw] mt-[5vw] flex">Expertise</p>
-      <div className="grid max-w-[90vw] grid-cols-4 gap-[2vw]">
+    <div className="bg-black text-black w-full min-h-screen flex flex-col items-center mb-[5vh]">
+      <p className="text-[4vw] sm:text-[5vw]  text-white w-full max-w-[90vw] mb-[3vw] mt-[5vw] flex justify-center ">Expertise</p>
+
+      {/* Grid Layout with responsive columns */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[2vw] max-w-[90vw]">
         {expertise_data.map((data, index) => (
-          <ExportiseCard key={index} data={data} setOpen={setOpen} open={open} setBrief={setBrief} />
+          <ExportiseCard
+            key={index}
+            data={data}
+            setOpen={setOpen}
+            open={open}
+            setBrief={setBrief}
+          />
         ))}
       </div>
-      <div className={`${open?'':'hidden'} backdrop-blur-lg min-h-screen fixed z-10 top-0 w-full text-white pt-[5vw] flex justify-center`}>
-        <ExpertiseBrief setOpen={setOpen} brief={brief}/>
+
+      {/* Brief modal popup */}
+      <div className={`${open ? '' : 'hidden'} backdrop-blur-lg min-h-screen fixed z-10 top-0 w-full text-white pt-[5vw] flex justify-center`}>
+        <ExpertiseBrief setOpen={setOpen} brief={brief} />
       </div>
     </div>
   );
--- components/expertise/expertise_card.jsx
+++ components/expertise/expertise_card.jsx
@@ -1,34 +1,45 @@
 import Link from "next/link"
 import CornerArrow from "../svg/corner-arrow"
 import { useState } from "react"
+import Image from "next/image";
 
-export default function ExportiseCard({data, open, setOpen, setBrief}){
-    return(
-        <div className="group min-h-[30vw] flex-1 flex flex-col bg-black pb-[1vw] text-white hover:bg-white hover:text-black transition duration-500 ease-out">
-            <div className={`bg-white flex-[2] mb-[1vw]`}>Image</div>
-            <div className="flex-[3] flex flex-col tracking-tighter group-hover:[&>*]:pl-[1vw] [&>*]:transition-all [&>*]:duration-500 [&>*]:ease-out" id="content">
-                <p className="transition duration-1000 ease-out text-[1vw] font-light">{data.dest}</p>
-                <p className="transition duration-1000 ease-out text-[1.5vw] font-semibold group-hover:underline decoration-2">{data.title}</p>
-                <p className="transition duration-1000 ease-out text-[1.2vw] font-normal">{data.person}</p>
-                <p className="transition duration-1000 ease-out text-[1vw] mt-[1vw]">
-                    {['400 M²','Pièces 8','4 Chambres','magasins','bus','terrace','garage','piscine','lake'].map((data, index)=>(
-                        <span key={index}>{data} <span className="text-[#737373]">/</span> </span>
+export default function ExportiseCard({ data, open, setOpen, setBrief }) {
+    return (
+        <div className="group flex flex-col min-h-[30vw] sm:min-h-[40vh] flex-1 bg-black pb-[1vw] text-white hover:bg-white hover:text-black transition duration-500 ease-out">
+            <div className="rounded-sm overflow-hidden">
+                <Image
+                    src={data.image}
+                    width={300}
+                    height={300}
+                    className="w-full h-[40vh] sm:h-[35vh] object-cover"
+                    alt="Logo"
+                    unoptimized
+                />
+            </div>
+
+            <div className="flex-1 flex flex-col tracking-tighter group-hover:[&>*]:pl-[1vw] [&>*]:transition-all [&>*]:duration-500 [&>*]:ease-out mt-4" id="content">
+                <p className="transition duration-1000 ease-out text-lg sm:text-xl mt-2 font-light">{data.dest}</p>
+                <p className="transition duration-1000 ease-out text-[1.5vw] sm:text-[2vw] font-semibold group-hover:underline decoration-2">{data.title}</p>
+                <p className="transition duration-1000 ease-out text-[1.2vw] sm:text-[1.8vw] font-normal">{data.person}</p>
+                <p className="transition duration-1000 ease-out text-[vw] sm:text-[1vw] mt-[1vw]">
+                    {['400 M²', 'Pièces 8', '4 Chambres', 'magasins', 'bus', 'terrace', 'garage', 'piscine', 'lake'].map((data, index) => (
+                        <span key={index}>{data} <span className="text-[#737373] ">/</span> </span>
                     ))}
                 </p>
-                <div className="flex-1 flex items-end">
-                    <button 
-                      onClick={()=>{
-                        setBrief(data);
-                        setOpen(true);
-                      }}
-                      className="border-solid border hover:text-white hover:bg-black border-[#737373] group-hover:border-black rounded-full min-w-1/2 flex py-[.3vw] px-[1vw] transition duration-500 ease-out">
-                        <p className="text-[1vw] mr-[.5vw]">More information</p>
-                        <p className="-rotate-90 [&>*]:size-[1vw] flex justify-center items-center">
-                            <CornerArrow/>
+                <div className="flex-1 flex items-end mt-2">
+                    <button
+                        onClick={() => {
+                            setBrief(data);
+                            setOpen(true);
+                        }}
+                        className="border-solid border hover:text-white hover:bg-black border-[#737373] group-hover:border-black rounded-full min-w-1/2 sm:min-w-[30%] flex py-[.3vw] px-[1vw] transition duration-500 ease-out mt-2  justify-center items-center">
+                        <p className="text-[1vw] sm:text-[1.2vw] mr-[.5vw]">More information</p>
+                        <p className="-rotate-90 [&>*]:size-[1vw] ">
+                            <CornerArrow />
                         </p>
                     </button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
